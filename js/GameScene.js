a44f1564ca92872ed742b1484002f3e99005723a
Diego Matos <diegovasconcelos62@gmail.com>
2020-07-25 12:38:15 -0300

    Dia 25

--- js/GameScene.js
+++ js/GameScene.js
@@ -11,38 +11,63 @@ class GameScene extends Phaser.Scene{
 
   create(){
     this.physics.world.setBounds(0, 0, 320, this.AlturaDoJogo-98)
+    this.GAP = 100;
+    this.tempoDeJogo = 0;
+    this.velocidadeDosCanos = -120;
 
     // Cenário
     this.background = this.add.tileSprite(0, this.AlturaDoJogo, 0, 0,"background_img");
     this.background.setOrigin(0, 1);
+    this.background.depth = 1;
 
     this.chao = this.add.tileSprite(0, this.AlturaDoJogo, 0, 0, "chao_img");
     this.chao.setOrigin(0, 1);
+    this.chao.depth = 3;
 
     // Jogador
     this.jogador = this.physics.add.sprite(this.LarguraDoJogo/2, this.AlturaDoJogo/2 + 20, "player_sprite");
     this.jogador.anims.play("Voo");
     this.jogador.setCollideWorldBounds();
+    this.jogador.body.setGravity(0, 1200);
     this.vida = 10;
-    // this.passaro.body.setGravity(0, 1200);
+    this.jogador.depth = 5;
 
     // Canos
     this.canos = this.physics.add.group();
-    // Cria com y aleatorio
-    let canoNorte = this.canos.create(posX, randomY, "pipeNorth_img");
-    // Cria logo em baixo do cano norte, com um espaço ( gap ) entre eles
-    let canoSul = this.canos.create(posX, randomY, "pipeSouth_img");
+    window.setInterval(this.adicionarCanos, 2000);
+    // this.time.addEvent({
+    //   delay: 2000,
+    //   callback: this.adicionarCanos,
+    //   callbackScope: this,
+    //   loop: true
+    // })
 
     // Interatividade
     this.input.on("pointerdown", this.voar, this);
+
+    // Colisoes
+    this.physics.add.overlap(this.jogador, this.canos, (objeto1, objeto2) => console.log(objeto2), null, this);
   }
 
-  update(){
+  update(time, deltaTime){
     this.background.tilePositionX += 0.5;
     this.chao.tilePositionX += 1;
   }
 
   voar(){
     this.jogador.setVelocity(0, -300);
+  }
+
+  adicionarCanos = () => {
+    let posY = Math.round(Math.random()* -220);
+    let canoNorte = this.canos.create(this.LarguraDoJogo, posY, "pipeNorth_img");
+    canoNorte.setOrigin(0);
+    canoNorte.setVelocity(this.velocidadeDosCanos, 0);
+    canoNorte.depth = 4;
+
+    let canoSul = this.canos.create(this.LarguraDoJogo, posY + canoNorte.displayHeight + this.GAP, "pipeSouth_img");
+    canoSul.setOrigin(0);
+    canoSul.setVelocity(this.velocidadeDosCanos, 0);
+    canoSul.depth = 2;
   }
 }
